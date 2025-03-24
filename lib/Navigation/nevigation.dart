8afc3916edc137df7bcf3455f164c499a8bbdfa8
AksharRami2007 <aksharrami878@gmail.com>
2025-03-24 17:04:05 +0530

    login Screen

--- lib/Navigation/nevigation.dart
+++ lib/Navigation/nevigation.dart
@@ -1,9 +1,16 @@
+import 'package:book_nexus/Navigation/routername.dart';
+import 'package:book_nexus/Screen/Auth/LogInScreen/LoginController.dart';
+import 'package:book_nexus/Screen/Auth/LogInScreen/LoginScreenWrapper.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
 class Pages {
   static List<GetPage> pages() {
     return [
-      
+       GetPage(
+        name: Routername.loginscreen,
+        page: () => const Loginscreenwrapper(),
+        binding: LogincontrollerBindings(),
+      ),
     ];
   }
 }
